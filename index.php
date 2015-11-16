9e9be9428cdde709d4f423d8b0626ea38bf0d5ff
Oricy <d.duvernois@student.isartdigital.com>
2015-11-16 18:26:17 +0100

    Add seo
    + prepare nav

--- index.php
+++ index.php
@@ -1,3 +1,75 @@
-<?php 
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jonathan Fechner - accueil</title>
+	<!-- Dublin Core -->
+	<meta charset="utf-8">
+	<meta name="DC.title" content="Jonathan Fechner">
+	<meta name="DC.creator" content="Jonathan Fechner">
+	<meta name="DC.subject" content="mots clefs" />
+	<meta name="DC.description" content="description du site" />
+	<meta name="DC.publisher" content="Auteur">
+	<meta name="DC.contributor" content="Contributeur">
+	<meta name="DC.date" content="date de création">
+	<meta name="DC.type" content="type de site">
+	<meta name="DC.format" content="website">
+	<meta name="DC.identifier" content="url du site">
+	<meta name="DC.language" content="fr-FR">
+	<meta name="DC.coverage" content="World">
+	<meta name="DC.rights" content="&copy; nom de l'entreprise ou de l'auteur">
+	<!-- END Dublin Core -->
+	<!-- Referencement -->
+	<meta name="description" content="description du site">
+	<meta name="keywords" content="mots clefs">
+	<meta name="author" content="auteur">
+	<meta name="robots" content="index">
+	<meta name="Indentifier-URL" content="url du site">
+	<!-- END Référencement -->
+	<!-- Open Graph -->
+	<meta property="og:title" content="titre du site">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="url du site">
+	<meta property="og:image" content="url de l'image à afficher">
+	<meta property="og:site_name" content="titre du site">
+	<meta property="og:description" content="texte de description à afficher sur les
+	réseaux sociaux">
+	<!-- END Open Graph-->
+	<link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<h1>Jonathan Fechner</h1>
+					<h2>Consultant – Formateur T.I.C</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-2 col-md-offset-1">
+					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+				</div>
+				<div class="col-md-2">
+					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+				</div>
+				<div class="col-md-2">
+					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+				</div>
+				<div class="col-md-2">
+					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+				</div>
+				<div class="col-md-2">
+					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+				</div>
+			</div>
+		</div>
+	</header>
+	<footer>
+		<?php
+			//require 'footer.php';
+		?>
+		<script type="text/javascript" href="lib/bootstrap/js/bootstrap.min.js"></script>
+		<script type="text/javascript" href="lib/jquery-2.1.1.min.js"></script>
+	</footer>
+</body>
+</html>

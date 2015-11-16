5e0e21014fdae307e6bc76ec088e1c5df8ab6af9
Gauthier Flichy <ga2015@flichy.biz>
2015-11-16 18:29:03 +0100

    Ajout de la page de compétences

---
+++ competences.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jonathan Fechner - Compétences</title>
+
+	<!-- Dublin Core -->
+	<meta name="DC.title" content="Jonathan Fechner">
+	<meta name="DC.creator" content="Jonathan Fechner">
+	<meta name="DC.subject" content="mots clefs" />
+	<meta name="DC.description" content="description du site" />
+	<meta name="DC.publisher" content="Jonathan Fechner">
+	<meta name="DC.contributor" content="Jonathan Fechner">
+	<meta name="DC.date" content="date de création">
+	<meta name="DC.type" content="type de site">
+	<meta name="DC.format" content="website">
+	<meta name="DC.identifier" content="url du site">
+	<meta name="DC.language" content="fr-FR">
+	<meta name="DC.coverage" content="World">
+	<meta name="DC.rights" content="&copy; Jonathan Fechner">
+	<!-- END Dublin Core -->
+	<!-- Referencement -->
+	<meta name="description" content="description du site">
+	<meta name="keywords" content="mots clefs">
+	<meta name="author" content="Jonathan Fechner">
+	<meta name="robots" content="index">
+	<meta name="Indentifier-URL" content="url du site">
+	<!-- END Référencement -->
+	<!-- Open Graph-->
+	<meta property="og:title" content="titre du site">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="url du site">
+	<meta property="og:image" content="url de l'image à afficher">
+	<meta property="og:site_name" content="titre du site">
+	<meta property="og:description" content="texte de description à afficher sur les
+	réseaux sociaux">
+	<!-- END Open Graph-->
+	<link rel="stylesheet" type="text/css" href="lib/boostrap/css/boostrap.min.css">
+	
+</head>
+<body>
+	<footer>
+		<?php 
+			//require 'footer.php';
+		?>	
+	</footer>
+</body>
+</html>

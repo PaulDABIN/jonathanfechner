2e8bfe234e9d4b1e1719480bf5d72d96455855fe
Oricy <d.duvernois@student.isartdigital.com>
2015-11-16 19:00:13 +0100

    add carousel
    edit css

---
+++ footer.php
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="lib/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="lib/bootstrap/js/bootstrap.min.js"></script>
--- index.php
+++ index.php
@@ -70,12 +70,52 @@
 			</div>
 		</div>
 	</header>
+
+	<content>
+		<div class="container-fluid" id="containCarousel">
+				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				  	<!-- Indicators -->
+				  	<ol class="carousel-indicators">
+					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				  	</ol>
+
+				  	<!-- Wrapper for slides -->
+				  	<div class="carousel-inner" role="listbox">
+				    	<div class="item active">
+				      		<img src="image/placeholder.png" class="carouselImg" alt="logo jonathan fechner" title="accueil">
+				      		<div class="carousel-caption">
+				        		<h2>ddddddd</h2>
+				        		<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.</p>
+				      		</div>
+				    	</div>
+				    	<div class="item">
+				      		<img src="image/placeholder.png" class="carouselImg" alt="logo jonathan fechner" title="accueil">
+				      		<div class="carousel-caption">
+				        		<h2>aaaaaaaaaaaaa</h2>
+				        		<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.</p>
+				      		</div>
+				    	</div>
+				  	</div>
+
+				  	<!-- Controls -->
+				  	<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				    	<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    	<span class="sr-only">Previous</span>
+				  	</a>
+				  	<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				    	<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    	<span class="sr-only">Next</span>
+				  	</a>
+				</div>
+		</div>
+	</content>
+
 	<footer>
 		<?php
-			//require 'footer.php';
+			require 'footer.php';
 		?>
-		<script type="text/javascript" href="lib/bootstrap/js/bootstrap.min.js"></script>
-		<script type="text/javascript" href="lib/jquery-2.1.1.min.js"></script>
 	</footer>
 </body>
 </html>

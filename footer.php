d52e838f39a5c40193b108750ee4ad80e2f16acc
Oricy <d.duvernois@student.isartdigital.com>
2015-11-16 19:35:06 +0100

    add footer reference

--- footer.php
+++ footer.php
@@ -1,2 +1,11 @@
+<div class="container-fluid footerContent">
+	
+</div>
+<div class="container-fluid">
+	<div class="text-center">
+		<small>Jonathan Fechner, consultant et formateur T.I.C - Mentions Légales - Plan du site - Tous droits réservés ©copyright 2015</small><br>
+		<small>Par Damien Duvernois, Shana Enkaoua, Rami El Khodry, Gauthier Flichy, Niki Gharabaghi,Paul Dabin</small>
+	</div>
+</div>
 <script type="text/javascript" src="lib/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="lib/bootstrap/js/bootstrap.min.js"></script>

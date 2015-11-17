21e4a2a2a83729e2ae724cc915834be4eafdfbf1
Oricy <d.duvernois@student.isartdigital.com>
2015-11-17 14:19:33 +0100

    add header file
    add social button icon (need href)

---
+++ header.php
@@ -0,0 +1,54 @@
+<header>
+	<div class="container topMargin">
+		<div class="row">
+		</div>
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="col-md-3">
+					<a href="index.php"><img src="image/placeholder.png" class="img-responsive" alt="logo jonathan fechner" title="accueil"></a>
+				</div>
+				<div class="col-md-5">
+					<h1>Jonathan Fechner</h1>
+					<h2>Consultant – Formateur T.I.C</h2>
+				</div>
+				<div class="col-md-1">
+					<a href="#">
+					  	<span class="fa fa-facebook-square socialIcon"></span>
+					</a>
+				</div>
+				<div class="col-md-1">
+					<a href="#">
+					  	<span class="fa fa-linkedin-square socialIcon"></span>
+					</a>
+				</div>
+				<div class="col-md-1">
+					<a href="#">
+					  	<span class="fa fa-twitter-square socialIcon"></span>
+					</a>
+				</div>
+				<div class="col-md-1">
+					<a href="#">
+					  	<span class="fa fa-youtube-square socialIcon"></span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="row navigationTitle">
+			<div class="col-md-2 col-md-offset-1">
+				<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+			</div>
+			<div class="col-md-2">
+				<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+			</div>
+			<div class="col-md-2">
+				<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+			</div>
+			<div class="col-md-2">
+				<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+			</div>
+			<div class="col-md-2">
+				<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
+			</div>
+		</div>
+	</div>
+</header>
--- index.php
+++ index.php
@@ -39,45 +39,9 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<header>
-		<div class="container topMargin">
-			<div class="row">
-				<div class="col-md-1">
-					<a href="#">
-					  	<span class="fa fa-facebook-square"></span>
-					</a>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1">
-					<div class="col-md-3">
-						<a href="index.php"><img src="image/placeholder.png" class="img-responsive" alt="logo jonathan fechner" title="accueil"></a>
-					</div>
-					<div class="col-md-9">
-						<h1>Jonathan Fechner</h1>
-						<h2>Consultant – Formateur T.I.C</h2>
-					</div>
-				</div>
-			</div>
-			<div class="row navigationTitle">
-				<div class="col-md-2 col-md-offset-1">
-					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
-				</div>
-				<div class="col-md-2">
-					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
-				</div>
-				<div class="col-md-2">
-					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
-				</div>
-				<div class="col-md-2">
-					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
-				</div>
-				<div class="col-md-2">
-					<a class="btn btn-default btn-block" href="#" role="button">Accueil</a>
-				</div>
-			</div>
-		</div>
-	</header>
+	<?php 
+	require 'header.php';
+	?>
 
 	<content>
 		<div class="container-fluid" id="containCarousel">
